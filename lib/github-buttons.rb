8b88b96385c98d963c9acfebd604afe0711c96c9
M. Maxwell Watson <hi@mmwtsn.com>
2014-01-07 01:20:07 -0500

    Initial port with passing spec

---
+++ lib/github-buttons.rb
@@ -0,0 +1,43 @@
+module GitHub
+  class Button
+    attr_accessor :user, :repo
+    def initialize(user, repo)
+      @user = user
+      @repo = repo
+    end
+
+    def style(type="follow", options={})
+      # Available GitHub Button types
+      types = ['fork', 'star', 'follow']
+
+      # Ensure button type exists
+      unless types.include? type
+        raise ArgumentError, %Q(#{type} button not available. Try "fork", "star" or "follow")
+      end
+
+      # Work around bug in GitHub Button repository
+      # Requesting "watch" actually returns "star"
+      # See: https://github.com/mdo/github-buttons/issues/42#issuecomment-19951052
+      if type == 'star'
+        type = 'watch'
+      end
+
+      # Ensure argument type is valid
+      unless type.is_a? String
+        raise ArgumentError, "Expected String, got #{type.class}"
+      end
+
+      # If no options are specified, pass none
+      if options.empty?
+        large = count = nil
+      end
+
+      # Format options as URL query parameters for final iFrame
+      if options[:large] then size = "&size=large" end
+      if options[:count] then count = "&count=true" end
+
+      # Return GitHub Button
+      %Q(<iframe src="http://ghbtns.com/github-btn.html?user=#{@user}&repo=#{@repo}&type=#{type}#{size}#{count}" allowtransparency="true" frameborder="0" scrolling="0" width="250" height="30"></iframe>)
+    end
+  end
+end
---
+++ spec/github-button_spec.rb
@@ -0,0 +1,64 @@
+require_relative '../lib/github-buttons.rb'
+
+describe GitHub::Button do
+  before(:all) do
+    @button = GitHub::Button.new('a-user', 'a-repository')
+  end
+
+  describe '#initialize' do
+    it 'gets the user name' do
+      @button.user.should eql 'a-user'
+    end
+
+    it 'gets the repository name' do
+      @button.repo.should eql 'a-repository'
+    end
+  end
+
+  describe '#style' do
+    it 'returns a correct string' do
+      @button.style.class.should eql String
+    end
+
+    it 'returns the correct string' do
+      @button.style.should eql %Q(<iframe src="http://ghbtns.com/github-btn.html?user=a-user&repo=a-repository&type=follow" allowtransparency="true" frameborder="0" scrolling="0" width="250" height="30"></iframe>)
+    end
+
+    it 'contains the user name' do
+      @button.style.match(%r(a-user)).should be_true
+    end
+
+    it 'contains the repo name' do
+      @button.style.match(%r(a-repository)).should be_true
+    end
+
+    # This is a bug in GitHub Buttons
+    # Requesting "watch" returns "star"
+    # See: https://github.com/mdo/github-buttons/issues/42#issuecomment-19951052
+    it 'accepts "star"' do
+      @button.style('star').match(%r(watch)).should be_true
+    end
+
+    it 'accepts "fork"' do
+      @button.style('fork').match(%r(fork)).should be_true
+    end
+
+    it 'accepts "follow"' do
+      @button.style('follow').match(%r(follow)).should be_true
+    end
+
+    it 'does not accept an Integer' do
+      expect{@button.style(12)}.to raise_error(ArgumentError)
+    end
+
+    it 'does not accept a non-standard String' do
+      b = GitHub::Button.new('mmwtsn', 'hipchat-emotodex')
+      expect{@button.style('bananas')}.to raise_error(ArgumentError)
+    end
+
+    it 'accepts a hash of options for size and count' do
+      b = @button.style('star', large: true, count: true)
+      b.match(/size=large.*count=true/).should be_true
+    end
+  end
+end
